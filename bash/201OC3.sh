422e31b31f36d03736e2ee294a596086cea5d5fc
tfichtner87@gmail.com <tfichtner87@gmail.com>
2023-04-27 01:26:31 +0000

    OC3

---
+++ bash/201OC3.sh
@@ -0,0 +1,14 @@
+#1/bin/bash
+
+# script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Travis Fichtner
+# Date of latest revision 4/26/23
+ # Purpose: Print a string to the terminal
+
+ # Main
+
+
+sudo last /var/log/wtmp
+sudo last -3
+
+ # End
