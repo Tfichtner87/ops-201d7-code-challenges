80a4f81c545232d6c5ab8f0620debd494d8cb47d
tfichtner87@gmail.com <tfichtner87@gmail.com>
2023-04-27 01:08:28 +0000

    first push

---
+++ bash/helloworld.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+
+# script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Travis Fichtner
+# Date of latest revision 4/26/23
+ # Purpose: Print a string to the terminal
+
+# Main
+
+echo "Hello World"
+
+
+# End
---
+++ bash/learnbash.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Learning bash
+# Use pound sign to comment out lines
+
+variable=4
+echo $variable
+myarray=("dog" "cat" "bunny"
+# Read command Allows user to input variable data
+echo "enter a number"
+read $number
+echo $number
+
+# if and else statements
+
+#if $number > 5
+#    then echo "number is greater than 5"
+#else $number < 5
+#    then echo "number is less than 5"
+
+#functions is to be called multiple times by only using function name
+function add()
+    echo "hello world"
+    echo "welcome"
