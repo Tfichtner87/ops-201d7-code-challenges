73a356eb923d663c21e2e0342e7757fa41e04e7f
travis fichtner <tfichtner87@gmail.com>
2023-05-03 19:47:13 -0400

    201OC6.sh

---
+++ bash/201OC6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# script: Ops 201 Class 04 Ops Challenge Solution
+# Author: Travis Fichtner
+# Date of latest revision 5/3/23
+ # Purpose: Conditionals
+
+ # Main
+
+directories=("dir1" "dir2" "dir3"
+for directory in "${directories[@]}: do
+if [! -d "directories"]: then
+mkdir "$directory"
+
+else
+echo "directory exists"
+fi
+done
