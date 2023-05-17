eacc6916d44f2f93fca1d981b1ef90f358c2f731
travis fichtner <tfichtner87@gmail.com>
2023-05-17 19:58:47 -0400

    OC12

--- bash/201OC12.ps1
+++ bash/201OC12.ps1
@@ -3,4 +3,8 @@
 # Date of latest revision 5/16/23
  # Purpose: Powershell IP Analysis
 
- 
+ function IP {ipconfig /all | Out-file -Filepath $file}
+ $file = "C:\Users\admin\Documents\network_report.txt
+ IP
+ Select-String -Path $file -Pattern IPv4
+ Remove-Item -Path $file
