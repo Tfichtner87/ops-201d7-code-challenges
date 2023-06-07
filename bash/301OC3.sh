dc9caf8aafc609180e433d21332c7612899313af
travis fichtner <tfichtner87@gmail.com>
2023-06-07 19:40:10 -0400

    301OC3

---
+++ bash/301OC3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# script: Ops 301 Class 03 Ops Challenge Solution
+# Author: Travis Fichtner
+# Date of latest revision 6/7/23
+ # Purpose: File Permissions
+
+ # Main
+
+ echo "enter a file"
+ ls 
+ read file
+ echo "Enter Your Name"
+ read my_var
+ echo "Welcome ${my_var}!"
+ echo "Enter Permissions"
+ read number
+ chmod $num $file 
+ echo "Welcome User"
+ ls -l $file
+
+#End
