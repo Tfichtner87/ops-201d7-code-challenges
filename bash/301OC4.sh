8048a3a3a3867276c488bb7ef2e8bb0bf1bc6c2d
travis fichtner <tfichtner87@gmail.com>
2023-06-13 19:35:10 -0400

    301OC4

---
+++ bash/301OC4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# script: Ops 301 Class 04 Ops Challenge Solution
+# Author: Travis Fichtner
+# Date of latest revision 6/13/23
+ # Purpose: Conditionals in Menu Systems
+
+ # Main
+
+ echo "Please Choose"
+echo "1 Hello World!"
+echo "2 Ping Self"
+echo "3 IP Info"
+echo "4 Exit"
+read a
+if [ $a = 1 ]
+    then echo "Hello World!"
+elif [ $a = 2 ] 
+    then ping -c 3 localhost
+elif [ $a = 3 ]
+    then ifconfig
+else [ $a = 4 ]
+    Exit
+fi 
+
+ 
