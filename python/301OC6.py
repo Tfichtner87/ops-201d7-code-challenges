84083f4498a735478a6f1a18b75b20e8fdab032e
travis fichtner <tfichtner87@gmail.com>
2023-06-19 20:07:56 -0400

    301OC6

---
+++ python/301OC6.py
@@ -0,0 +1,13 @@
+import os
+whoami = os.popen('whoami').read()
+ip = os.popen('ip a').read()
+lshw = os.popen('lshw-short').read()
+
+print("Output of 'whoami' command:")
+print(whoami)
+
+print("Output of 'ip a' command")
+print(ip)
+
+print("\Output of 'lshw -short' command:")
+print(lshw)
