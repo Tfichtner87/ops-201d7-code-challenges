904ff02106313557ba634b3c5b5d4ef3268662a6
travis fichtner <tfichtner87@gmail.com>
2023-06-26 20:58:43 -0400

    301OC8

---
+++ python/301OC8.py
@@ -0,0 +1,7 @@
+players = ["lebron","charles barkley", "larry bird", "shaq", "kobe", "magic johnson", "michael jordan", "blake griffen", "kyrie irving", "steph curry"] 
+print(players[3])
+print(players[4]+""+ players[9])
+players[6] = 'muggsy'
+print(players)
+
+#end
