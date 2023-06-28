fcf8aacaa080718a21ab1f03d1e844e3a27b1e3b
travis fichtner <tfichtner87@gmail.com>
2023-06-28 19:35:49 -0400

    301OC9

---
+++ python/301OC9.py
@@ -0,0 +1,8 @@
+for num in range (1, 101):
+    print (num)
+    if num % 3 == 0:
+        print ("Fizz")
+    elif num % 5 == 0:
+        print ("Buzz")
+    elif num % 3 and  5:
+        print ("Fizzbuzz")
